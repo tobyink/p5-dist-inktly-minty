6717e930c38a15d07c100121687c789e7cc303e0
Toby Inkster <mail@tobyinkster.co.uk>
2023-01-26 12:31:56 +0000

    Copyright dates, etc

--- lib/Dist/Inktly/Cervine.pm
+++ lib/Dist/Inktly/Cervine.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Inktly::Cervine;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.004';
+our $VERSION   = '0.005';
 
 use Text::Template;
 
@@ -116,7 +116,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2013-2022 by Toby Inkster.
+This software is copyright (c) 2013-2023 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
